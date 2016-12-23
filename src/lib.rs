3ed922f9b7276b2a5335053e1ee3fbb978f9b111
Lucille Blumire <llblumire@gmail.com>
2016-12-23 16:10:46 +0000

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -190,17 +190,6 @@ macro_rules! split_to_array {
 ///
 /// Returns `None` when length is longer than slice.
 macro_rules! split_to_array_mut {
-        fn slice_to_array<T>(source: &[T]) -> Option<&[T; $len]> {
-            if source.len() < $len {
-                None
-            } else {
-                Some(unsafe {
-                    slice_to_array_unchecked!(source, $len)
-                })
-            }
-        }
-        
-        slice_to_array($source)
     ($source:expr, $len:expr) => {{
         fn split_to_array_mut<T>(source: &mut [T]) -> Option<(&mut [T; $len], &mut [T])> {
             if source.len() < $len {
@@ -213,4 +202,51 @@ macro_rules! split_to_array_mut {
         
         split_to_array_mut($source)
     }}
+}
+
+
+#[test]
+fn slice_to_array_test() {
+    let source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    assert_eq!(slice_to_array!(&source[..], 10), Some(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    assert_eq!(slice_to_array!(&source[..], 5), Some(&[1, 2, 3, 4, 5]));
+    assert_eq!(slice_to_array!(&source[..5], 5), Some(&[1, 2, 3, 4, 5]));
+    assert_eq!(slice_to_array!(&source[..5], 6), None);
+
+    let source = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    assert_eq!(slice_to_array!(&source[..], 10), Some(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    assert_eq!(slice_to_array!(&source[..], 5), Some(&[1, 2, 3, 4, 5]));
+    assert_eq!(slice_to_array!(&source[..5], 5), Some(&[1, 2, 3, 4, 5]));
+    assert_eq!(slice_to_array!(&source[..5], 6), None);
+}
+
+#[test]
+fn slice_to_array_mut_test() {
+    let mut source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    {
+        if let Some(arr) = slice_to_array_mut!(&mut source[3..7], 4) {
+            arr[3] = 100;
+        }
+    }
+    assert_eq!(source, [1, 2, 3, 4, 5, 6, 100, 8, 9, 10]);
+}
+
+
+#[test]
+fn split_to_array_test() {
+    let source = [1, 2, 3, 4, 5];
+    assert_eq!(split_to_array!(&source[..], 3), Some((&[1, 2, 3], &source[3..])));
+    assert_eq!(split_to_array!(&source[..], 6), None);
+}
+
+#[test]
+fn split_to_array_mut_test() {
+    let mut source = [1, 2, 3, 4, 5];
+    {
+        if let Some((arr, end)) = split_to_array_mut!(&mut source, 3) {
+            arr[1] = 100;
+            end[1] = 200;
+        }
+    }
+    assert_eq!(source, [1, 100, 3, 4, 200]);
 }
